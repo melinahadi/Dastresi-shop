4417c12c063ad26ae501bdca6e46842c5794e708
Melina Hadi <melinahadi20@gmail.com>
2025-04-13 07:22:40 -0700

    تغییرات جدید: اضافه کردن اسلایدر محصول

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/Dastresi-shop/", // 👈 خیلی مهم!
+
     server: {
         proxy: {
             "/api": {
